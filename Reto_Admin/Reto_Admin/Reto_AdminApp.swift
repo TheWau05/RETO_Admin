805f9bab1c705362071ef7fc7cb20afed6e76614
Jose Mauricio <Motrezt@gmail.com>
2025-09-22 16:51:50 -0600

    Inicio de Admin

---
+++ Reto_Admin/Reto_Admin/Reto_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reto_AdminApp.swift
+//  Reto_Admin
+//
+//  Created by Mauricio on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct Reto_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
